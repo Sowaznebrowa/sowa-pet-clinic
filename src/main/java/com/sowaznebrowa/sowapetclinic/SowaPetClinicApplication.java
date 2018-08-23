ab13c67b958d0c5bf919a3f0ce6111338d719fd9
Sowaznebrowa <przemektarkowski@wp.pl>
2018-08-23 22:34:00 +0200

    Springboot Initializer files

---
+++ src/main/java/com/sowaznebrowa/sowapetclinic/SowaPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.sowaznebrowa.sowapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SowaPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SowaPetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sowaznebrowa/sowapetclinic/SowaPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sowaznebrowa.sowapetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SowaPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
