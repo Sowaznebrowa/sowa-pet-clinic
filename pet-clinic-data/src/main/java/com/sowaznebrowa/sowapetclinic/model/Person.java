f0b68b55f7bb8dedeac4f33e481ad128af1b8576
Sowaznebrowa <przemektarkowski@wp.pl>
2018-08-29 22:37:47 +0200

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/com/sowaznebrowa/sowapetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/sowaznebrowa/sowapetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.sowaznebrowa.sowapetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String FirstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/sowaznebrowa/sowapetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/sowaznebrowa/sowapetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.sowaznebrowa.sowapetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
