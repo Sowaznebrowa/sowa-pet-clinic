4b3c23babcc80b2cbbf310e4f2db0f3071990dbb
Sowaznebrowa <przemektarkowski@wp.pl>
2018-09-10 22:35:19 +0200

    Added ID generation in Map Impl. Closes #24

--- pet-clinic-data/src/main/java/com/sowaznebrowa/sowapetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/sowaznebrowa/sowapetclinic/services/map/AbstractMapService.java
@@ -3,14 +3,11 @@ package com.sowaznebrowa.sowapetclinic.services.map;
 import com.sowaznebrowa.sowapetclinic.model.BaseEntity;
 import com.sowaznebrowa.sowapetclinic.services.CrudService;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public abstract class AbstractMapService<T extends BaseEntity,ID> implements CrudService<T,ID>{
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> implements CrudService<T,ID>{
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     @Override
     public T findById(ID id) {
@@ -19,6 +16,15 @@ public abstract class AbstractMapService<T extends BaseEntity,ID> implements Cru
 
     @Override
     public T save(T object) {
+
+        if(object != null) {
+            if(object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
         map.put((ID) object.getId(), object);
         return object;
     }
@@ -37,5 +43,17 @@ public abstract class AbstractMapService<T extends BaseEntity,ID> implements Cru
     public void deleteById(ID id) {
         map.remove(id);
 
+    }
+
+    private Long getNextId(){
+        Long nextId = null;
+
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
