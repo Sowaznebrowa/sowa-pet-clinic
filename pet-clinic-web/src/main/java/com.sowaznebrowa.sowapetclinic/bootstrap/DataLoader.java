a6e3ce70052a595f7fb3aa80b61615be3820c18d
Sowaznebrowa <przemektarkowski@wp.pl>
2018-09-10 22:07:04 +0200

    Added Owners List and uploaded controller. Closes #17

--- pet-clinic-web/src/main/java/com.sowaznebrowa.sowapetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com.sowaznebrowa.sowapetclinic/bootstrap/DataLoader.java
@@ -28,7 +28,7 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(1L);
+        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Gienanme");
         ownerService.save(owner2);
--- pet-clinic-web/src/main/java/com.sowaznebrowa.sowapetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com.sowaznebrowa.sowapetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package com.sowaznebrowa.sowapetclinic.controllers;
 
+import com.sowaznebrowa.sowapetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-        public String listOwners(){
+        public String listOwners(Model model){
+
+            model.addAttribute("owners", ownerService.findAll());
+
             return "owners/index";
         }
 }
